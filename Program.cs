39c1396b1d0239df62ace281bc78235061ce42a6
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-06-01 12:21:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//Beni Yaratan Elbet Yolumu Gösterir .Şuara 78
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float number1;
+            float number2;
+            float sum;
+
+            Console.WriteLine("Please enter first number :");
+            number1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter second number :");
+            number2 = float.Parse(Console.ReadLine());
+
+            sum = number1 + number2;
+
+            Console.WriteLine("Sum of two number :"+" "+ sum);
+
+            Console.Read();
+        
+        }
+    }
+}
